6f619eca3942f713541ef8041dcc82f5393cd450
Yeshua235 <joshuaayanlowo235@gmail.com>
2023-10-16 03:50:10 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
 * rev_string - reverses a string
- 
 * _putchar - print each character
 * @s: char to check
-*
 * Description: This will reverse a string
 * Return: 0 is success
 */
